5d7d4e7d90ad2e62f9b7b792e2b86b6cd1a5c9ef
Yago-Avella <yagoserer6@gmail.com>
2025-01-08 15:41:11 +0100

    Cambiar el fondo

--- src/snakegame/SnakeGame.java
+++ src/snakegame/SnakeGame.java
@@ -30,7 +30,7 @@ public class SnakeGame extends JPanel implements ActionListener {
     public void paint(Graphics g) {
         g.setColor(color(40, 40, 40));//Color de fondo
         g.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
-        g.setColor(color(255, 255, 255));//Color de líneas
+        g.setColor(color(255, 255, 255));//Color de línea
 
         for (int xx = 0; xx <= WIDTH * SCALE; xx += SCALE) {
             g.drawLine(xx, 0, xx, HEIGHT * SCALE);
